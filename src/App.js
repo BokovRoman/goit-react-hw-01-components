4ffc062c9d8e268d1a1e09c2bc9ffc9ac1a03488
BokovRoman <r.bokov1991@gmail.com>
2021-05-28 22:38:36 +0300

    add task-1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
-function App() {
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+import React from 'react';
+import Profile from './components/Profile';
+import profiles from './user.json';
+// console.log(profiles);
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Profile
+        name={profiles.name}
+        tag={profiles.tag}
+        location={profiles.location}
+        avatar={profiles.avatar}
+        stats={profiles.stats}
+        followers={profiles.stats.followers}
+        views={profiles.stats.views}
+        likes={profiles.stats.likes}
+      />
     </div>
   );
-}
+};
 
 export default App;
