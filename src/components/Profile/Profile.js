2861d1d3083cf79ba811c321c7a3a2793a98d68e
BokovRoman <r.bokov1991@gmail.com>
2021-06-02 21:38:24 +0300

    adds styles to task-1

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,33 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import defaultImage from './default-image.jpg';
+import styles from './Profile.module.css';
+// console.log(styles);
 
 const Profile = (props) => {
     return (
-        <div class="profile">
+        <div className={styles.profile}>
             <div class="description">
             <img
-                src={props.avatar}
-                alt="Аватар пользователя"
-                class="avatar"
+                    src={props.avatar}
+                    alt="Аватар пользователя"
+                    className={styles.avatar}
             />
-            <p class="name">{props.name}</p>
-            <p class="tag">{props.tag}</p>
-            <p class="location">{props.location}</p>
+            <p className={styles.name}>{props.name}</p>
+            <p className={styles.tag}>{props.tag}</p>
+            <p className={styles.location}>{props.location}</p>
             </div>
 
-            <ul class="stats">
-            <li>
-                <span class="label">Followers</span>
-                <span class="quantity">{props.stats.followers}</span>
+            <ul className={styles.stats}>
+            <li className={styles.item}>
+                    <span className={styles.label}>Followers</span>
+                    <span className={styles.quantity}>{props.stats.followers}</span>
             </li>
-            <li>
-                <span class="label">Views</span>
-                <span class="quantity">{props.stats.views}</span>
+            <li className={styles.item}>
+                <span className={styles.label}>Views</span>
+                <span className={styles.quantity}>{props.stats.views}</span>
             </li>
-            <li>
-                <span class="label">Likes</span>
-                <span class="quantity">{props.stats.likes}</span>
+            <li className={styles.item}>
+                <span className={styles.label}>Likes</span>
+                <span className={styles.quantity}>{props.stats.likes}</span>
             </li>
             </ul>
         </div >
