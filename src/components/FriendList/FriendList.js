a4adffcde20fb8f954e0e1769d00f3c8b8e0d677
BokovRoman <r.bokov1991@gmail.com>
2021-06-02 22:42:32 +0300

    adds styles to task-3

--- src/components/FriendList/FriendList.js
+++ src/components/FriendList/FriendList.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from './FriendList.module.css';
 
 const FriendList = (props) => {
     return (
-        <ul class="friend-list">
+        <ul className={styles.list}>
             {props.friends.map((friend) => (
-                <li class="item" key={friend.id}>
-                    <span class="status">{friend.isOnline}</span>
-                    <img class="avatar" src={friend.avatar} alt={friend.name} width="48" />
-                    <p class="name">{friend.name}</p>
+                <li className={styles.item} key={friend.id}>
+                    <span className={friend.isOnline?styles.on:styles.off}>{friend.isOnline}</span>
+                    <img className={styles.avatar} src={friend.avatar} alt={friend.name} width="48" />
+                    <p className={styles.name}>{friend.name}</p>
                 </li>
             ))}
             {/* <!-- Произвольное кол-во FriendListItem, в зависимости от кол-ва объектов в массиве --> */}
